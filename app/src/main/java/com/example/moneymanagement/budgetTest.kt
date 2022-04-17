298506522b49db4f90d66b4c454c6a206b1c2aa2
CB <brandon.cozart2000@gmail.com>
2022-04-17 02:16:05 -0400

    Open Screen
    On Create will start with welcome screen

--- app/src/main/java/com/example/moneymanagement/budgetTest.kt
+++ app/src/main/java/com/example/moneymanagement/budgetTest.kt
@@ -3,6 +3,7 @@ package com.example.termproject_a
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.example.moneymanagement.R
 import java.lang.Exception
 import kotlin.math.absoluteValue
 
